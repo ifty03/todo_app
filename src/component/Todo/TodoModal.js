b80302572b394414ae849a2ce5ae88e056ec563a
ifty03 <ashikulislamb84@gmail.com>
2022-05-19 02:34:20 +0600

    modal impliment

--- src/component/Todo/TodoModal.js
+++ src/component/Todo/TodoModal.js
@@ -5,13 +5,26 @@ const TodoModal = () => {
     e.preventDefault();
     const name = e.target.name.value;
     const description = e.target.description.value;
-    console.log(description, name);
+    const todo = { name, description };
+    fetch("http://localhost:5000/todo", {
+      method: "POST",
+      headers: { "content-type": "application/json" },
+      body: JSON.stringify(todo),
+    })
+      .then((res) => res.json())
+      .then((data) => console.log("data", data));
   };
   return (
     <div className="">
       <input type="checkbox" id="todo-modal" class="modal-toggle" />
       <div class="modal modal-bottom sm:modal-middle bg-gray-700">
         <div class="modal-box">
+          <label
+            for="todo-modal"
+            class="btn btn-sm btn-circle btn-accent absolute right-2 top-2"
+          >
+            ✕
+          </label>
           <h3 className="text-center text-2xl text-accent font-semibold mb-5">
             Add A Todo!
           </h3>
